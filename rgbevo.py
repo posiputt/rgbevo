a977d394c9539fc68ede82f01b95ba947e9ab9a4
posiputt <i@posiputt.de>
2015-09-30 19:39:00 +0200

    added generation counter. all directions now have fitness function

--- rgbevo.py
+++ rgbevo.py
@@ -30,7 +30,9 @@ class cell:
 class world:
     def __init__(self, max_genome_length, size_x=30, size_y=10, mutation_rate=0.01):
         self.mgl = max_genome_length
-        # self.optimal_genome = self.mgl
+        self.optimal_genome = 0xf
+        self.change_optimal_at = 300000
+        self.change_index = 0
         self.size_x = size_x
         self.size_y = size_y
         self.mr = mutation_rate
@@ -41,12 +43,21 @@ class world:
                 self.worldmap[row].append(cell(self.mgl))
         self.worldstring = self.gen_worldstring()
         self.direction_stat = {'n':0, 'w':0, 's':0, 'e':0}
+        self.generations = 0
         
     def run(self):
         while (True):
             self.tic()
             print self.worldstring
-            print "n: %s\tw: %s\ts: %s\te: %s" % (self.direction_stat['n'], self.direction_stat['w'], self.direction_stat['s'], self.direction_stat['e'])
+            print "optimal: %2x (%i of %i), generations: %i\nn: %s, w: %s, s: %s, e: %s" % (
+                self.optimal_genome,
+                self.change_index,
+                self.change_optimal_at,
+                self.generations, self.direction_stat['n'],
+                self.direction_stat['w'],
+                self.direction_stat['s'],
+                self.direction_stat['e']
+            )
             time.sleep(0.1)
 
     def gen_worldstring(self):
@@ -54,7 +65,7 @@ class world:
         worldstring = ''
         for row in self.worldmap:
             for cell in row:
-                worldstring += "%x" % cell.get_genome()
+                worldstring += "%2x" % cell.get_genome()
             worldstring += "\n"
         self.worldstring = worldstring
 
@@ -69,35 +80,47 @@ class world:
                 if child[0] == 'n':
                     rindex = rowkey-1
                     if not rindex < 0:
-                        rival_genome = wm[rindex][cellkey].get_genome()
-                        if not rival_genome > cell.get_genome():
+                        rival_genome = self.worldmap[rindex][cellkey].get_genome()
+                        if not abs(self.optimal_genome - rival_genome) < abs(self.optimal_genome - cell.get_genome()):
                             wm[rindex][cellkey].set_genome(child[1])
                     else:
                         pass
                 elif child[0] == 'w':
                     cindex = cellkey+1
                     if not cindex >= self.size_x:
-                        wm[rowkey][cindex].set_genome(child[1])
+                        rival_genome = self.worldmap[rowkey][cindex].get_genome()
+                        if not abs(self.optimal_genome - rival_genome) < abs(self.optimal_genome - cell.get_genome()):
+                            wm[rowkey][cindex].set_genome(child[1])
                     else:
                         pass
                 if child[0] == 's':
                     rindex = rowkey+1
                     if not rindex >= self.size_y:
-                        wm[rindex][cellkey].set_genome(child[1])
+                        rival_genome = self.worldmap[rindex][cellkey].get_genome()
+                        if not abs(self.optimal_genome - rival_genome) < abs(self.optimal_genome - cell.get_genome()):
+                            wm[rindex][cellkey].set_genome(child[1])
                     else:
                         pass
                 if child[0] == 'e':
                     cindex = cellkey-1
                     if not cindex < 0:
-                        wm[rowkey][cindex].set_genome(child[1])
+                        rival_genome = self.worldmap[rowkey][cindex].get_genome()
+                        if not abs(self.optimal_genome - rival_genome) < abs(self.optimal_genome - cell.get_genome()):
+                            wm[rowkey][cindex].set_genome(child[1])
                     else:
                         pass
                 self.direction_stat[child[0]] += 1
+                if child[1] == self.optimal_genome:
+                    self.change_index += 1
+                    if self.change_index >= self.change_optimal_at:
+                        self.optimal_genome = random.choice(range(self.mgl))
+                        self.change_index = 0
         self.worldmap =  wm
         self.gen_worldstring()
+        self.generations += 1
 
 if __name__ == '__main__':
-    earth = world(0x10, 70, 30, 0.01)
+    earth = world(0x10, 75, 40, 0.00001)
     earth.run()
     time.sleep(1)
     while(True):
